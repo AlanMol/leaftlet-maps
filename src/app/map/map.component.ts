c51c60ea34c49e93a747d464a9ed00ccb0245ca1
AlanMol <molohua.alan.07@hotmail.com>
2020-09-27 22:54:44 -0500

    add leaftlet map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+  private map;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 18.804694, -97.026694 ],
+      zoom: 16
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+  }
+
+}
